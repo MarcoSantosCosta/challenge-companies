50f7aa07dc7c201a2449ffede4db4c16d8de496c
Marco Tulio Costa <marcotscosta@gmail.com>
2023-06-16 09:38:35 -0300

    feat(validation): data validator exception handler

---
+++ src/main/java/com/accenture/challengecompanies/presentation/exceptions/InputDataValidationExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.accenture.challengecompanies.presentation.exceptions;
+
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class InputDataValidationExceptionHandler {
+
+    private static final String ERROR_MESSAGE = "Invalid field(s)";
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public InvalidFieldsErrorsResponse handleValidationException(MethodArgumentNotValidException ex) {
+        List<InvalidFieldMessageError> errors = new ArrayList<>();
+
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(new InvalidFieldMessageError(error.getField(), error.getDefaultMessage()));
+        }
+
+        return new InvalidFieldsErrorsResponse(ERROR_MESSAGE, errors);
+    }
+
+    public record InvalidFieldsErrorsResponse(String error, List<InvalidFieldMessageError> errorsMessages) {
+    }
+
+    public record InvalidFieldMessageError(String field, String message) {
+    }
+}
