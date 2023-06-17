921d4c5c4aa9db510ed791af6d0b08f7dcc4bbcc
Marco Tulio Costa <marcotscosta@gmail.com>
2023-06-17 17:38:54 -0300

    fix(handlers): fix return code when throws exceptions

--- src/main/java/com/accenture/challengecompanies/presentation/handlers/ElementNotFoundExceptionHandler.java
+++ src/main/java/com/accenture/challengecompanies/presentation/handlers/ElementNotFoundExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.accenture.challengecompanies.presentation.handlers;
 
 import com.accenture.challengecompanies.domain.exceptions.ElementNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ElementNotFoundExceptionHandler {
 
     private static final String ERROR_MESSAGE = "Not found element";
--- src/main/java/com/accenture/challengecompanies/presentation/handlers/InputDataValidationExceptionHandler.java
+++ src/main/java/com/accenture/challengecompanies/presentation/handlers/InputDataValidationExceptionHandler.java
@@ -1,14 +1,17 @@
 package com.accenture.challengecompanies.presentation.handlers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestControllerAdvice
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InputDataValidationExceptionHandler {
 
     private static final String ERROR_MESSAGE = "Invalid field(s)";
